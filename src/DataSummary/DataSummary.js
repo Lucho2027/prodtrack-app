f39a7095bf501bb1eb861644c81baed9b33ecf5e
Luis Rodriguez <luis01.ro@gmail.com>
2020-02-13 13:33:46 -0600

    "Styling completed need mediaqris and dashboard"

--- src/DataSummary/DataSummary.js
+++ src/DataSummary/DataSummary.js
@@ -1,35 +1,37 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import ProdtrackContext from "../ProdtrackContext"
-import { Link } from "react-router-dom"
-import DataItem from "../DataItem/DataItem"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import ProdtrackContext from "../ProdtrackContext";
+import { Link } from "react-router-dom";
+import DataItem from "../DataItem/DataItem";
 // import data from "../data"
-import "./DataSummary.css"
+import "./DataSummary.css";
 export default class DataSummary extends Component {
-	static propTypes = {
-		data: PropTypes.arrayOf(
-			PropTypes.shape({
-				id: PropTypes.oneOf([PropTypes.number, PropTypes.string]).isRequired
-			})
-		)
-	}
-	static contextType = ProdtrackContext
+  static propTypes = {
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.oneOf([PropTypes.number, PropTypes.string]).isRequired
+      })
+    )
+  };
+  static contextType = ProdtrackContext;
 
-	render() {
-		const { data } = this.context
+  render() {
+    const { data } = this.context;
 
-		return (
-			<div className="data-summary">
-				<h1>Data Entry summary</h1>
-				<div className="data-item">
-					{data.map(item => (
-						<DataItem key={item.id} {...item} item={item} />
-					))}
-				</div>
-				<Link to="/dataentry">
-					<h4> Make a new Entry</h4>
-				</Link>
-			</div>
-		)
-	}
+    return (
+      <div className="data-summary">
+        <h1 className="header-intro">Data Entry summary</h1>
+        <div className="data-item">
+          {data.map(item => (
+            <DataItem key={item.id} {...item} item={item} />
+          ))}
+        </div>
+        <div className="ending">
+          <Link to="/dataentry">
+            <button className="todataentry-summ">Make a new Entry</button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-	// `https://agile-lake-97841.herokuapp.com/api/dataentry`
-	API_ENDPOINT: `http://localhost:8000/api/dataentry`,
-	API_KEY: process.env.REACT_APP_API_KEY
-}
+  // `http://localhost:8000/api/dataentry`
+  API_ENDPOINT: `https://agile-lake-97841.herokuapp.com/api/dataentry`,
+  API_KEY: process.env.REACT_APP_API_KEY
+};
